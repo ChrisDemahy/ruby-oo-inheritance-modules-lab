36638200bf943c2a62eea2f6dadf20ebd2816519
ChrisDemahy <demahyc@gmail.com>
2020-10-14 17:20:16 -0500

    Finished main refactoring. Doing bonus next.

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,8 @@ require 'pry'
 class Song
   extend Memorable
   extend Findable 
+  include Paramable
+
 
   attr_accessor :name
   attr_reader :artist
@@ -22,7 +24,7 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
